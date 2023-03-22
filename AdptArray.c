49d56830c10c794fd44a8eb50493df29ee8eff48
ohadwolfman <ohad1696@gmail.com>
2023-03-22 18:17:04 +0200

    no leacking, stil 3 errors

--- AdptArray.c
+++ AdptArray.c
@@ -1,2 +1,84 @@
-#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "AdptArray.h"
 
+typedef struct AdptArray_{
+    int ArrSize;
+    PElement* pElemArr;
+    DEL_FUNC delFunc;
+    COPY_FUNC copyFunc;
+    PRINT_FUNC prtFunc;
+}AdptArray;
+
+PAdptArray CreateAdptArray(COPY_FUNC copyFunc_, DEL_FUNC delFunc_,PRINT_FUNC prtFunc){
+    PAdptArray pArr = (PAdptArray)malloc(sizeof(AdptArray));
+    if (pArr == NULL)
+        return NULL;
+    pArr->ArrSize = 0;
+    pArr->pElemArr = NULL;
+    pArr->delFunc = delFunc_;
+    pArr->copyFunc = copyFunc_;
+    pArr->prtFunc = prtFunc;
+    return pArr;
+}
+
+Result SetAdptArrayAt(PAdptArray pArr, int idx, PElement pNewElem){
+    PElement* newpElemArr;
+    if (pArr == NULL)
+        return FAIL;
+
+    if (idx >= pArr->ArrSize)
+    {
+        if ((newpElemArr = (PElement*)calloc((idx + 1), sizeof(PElement))) == NULL)
+            return FAIL;
+        memcpy(newpElemArr, pArr->pElemArr, (pArr->ArrSize) * sizeof(PElement));
+        free(pArr->pElemArr);
+        pArr->pElemArr = newpElemArr;
+    }
+    // Delete Previous Elem
+    if((pArr->pElemArr)[idx]) {
+        pArr->delFunc((pArr->pElemArr)[idx]);
+        (pArr->pElemArr)[idx] = pArr->copyFunc(pNewElem);
+    }
+    else{
+        (pArr->pElemArr)[idx] = pArr->copyFunc(pNewElem);
+    }
+    // Update Array Size
+    pArr->ArrSize = (idx >= pArr->ArrSize) ? (idx + 1) : pArr->ArrSize;
+    return SUCCESS;
+}
+
+void DeleteAdptArray(PAdptArray pArr) {
+    int i;
+    if (pArr == NULL)
+        return;
+    for (i = 0; i < pArr->ArrSize; ++i) {
+        if((pArr->pElemArr)[i]!=NULL){
+            pArr->delFunc((pArr->pElemArr)[i]);
+        }
+    }
+    free(pArr->pElemArr);
+    free(pArr);
+}
+
+PElement GetAdptArrayAt(PAdptArray pArr, int idx){
+    if (pArr == NULL || index < 0 || idx >= (pArr->ArrSize))
+        return NULL;
+    return pArr->pElemArr[idx];
+}
+
+int GetAdptArraySize(PAdptArray pArr){
+    if (pArr == NULL)
+        return 0;
+    return pArr->ArrSize;
+}
+
+void PrintDB(PAdptArray pArr){
+    if (pArr == NULL)
+        return;
+    for (int i = 0; i < pArr->ArrSize; i++) {
+        if (pArr->pElemArr[i]) {
+            pArr->prtFunc(pArr->pElemArr[i]);
+        }
+    }
+}
--- Demo.c
+++ Demo.c
@@ -1,6 +1,6 @@
 /**
  * Demo program for ADT exercise.
- * 
+ *
  * Author: Evgeny Hershkovitch Neiterman
  * Since : 2023-02
  */
@@ -14,19 +14,19 @@
 #include <assert.h>
 
 int main() {
-
 	pbook b1 = creat_book("harry Potter",12345) ;
 	pbook b2= creat_book("C intro", 45678) ;
+
 	PAdptArray mybooks = CreateAdptArray(copy_book,delete_book,print_book);
 	SetAdptArrayAt(mybooks,3,b1);
 	SetAdptArrayAt(mybooks,5,b2);
 	printf("the size is %d\n",GetAdptArraySize(mybooks));  //should print 6
 	pbook b = GetAdptArrayAt(mybooks,4); // should return null;
 	assert(b==NULL); // doesn't fail
+
 	b = GetAdptArrayAt(mybooks,3);
 	printf("the book is %s\n",b->name); //should print "Harry Potter"
 	PrintDB(mybooks);
-
 	pperson p1 = creat_person("Harry","Potter", 934);
 	pperson p2 = creat_person("Ron","Weasley", 789);
 	PAdptArray HP_caracters = CreateAdptArray(copy_person,delete_person,print_person);
@@ -36,7 +36,9 @@ int main() {
 	PrintDB(HP_caracters); // prints:
 	//first name: Harry last name: Potter id: 934
         //first name: Ron last name: Weasley id: 789
-	
+
+    printf("%d\n",4);
+
 	DeleteAdptArray(mybooks);
 	DeleteAdptArray(HP_caracters);
 	delete_book(b1);
